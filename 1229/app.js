4127a76060844d4d4734e62fd8a1f37ee6bc8263
nopecho <ch.jooon@gmail.com>
2021-12-29 10:22:16 +0900

    추가 : 배열.find 함수

--- 1229/app.js
+++ 1229/app.js
@@ -20,5 +20,16 @@ while( foundIndex != -1){
 console.log(serchedIndexes)
 
 const sercheAnimal4 = animals.lastIndexOf('cat')
-console.log(sercheAnimal4)
+console.log(sercheAnimal4) //7
 //[배열].indexOf( "검색할 요소" , 검색 위치 ) => index값 리턴 (검색 요소가 없으면 -1 리턴)
+
+
+const fruits = ['apple', 'banana', 'orange', 'watermelon']
+
+const found = fruits.find(fruit => fruit.length > 6) 
+console.log(found)
+
+const foundI = fruits.findIndex(s => s.length > 6)
+console.log(foundI)
+
+//[배열].find((콜백함수)) //배열의 콜백함수 리턴값에 해당되는 배열요소 반환
